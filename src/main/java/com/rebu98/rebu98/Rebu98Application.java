08e40bab79c3a169b4a03b46a5ec4f40532adf4d
leodipaula <leofernandes9@gnail.com>
2025-04-25 11:04:31 -0300

    Estrutura inicial

---
+++ src/main/java/com/rebu98/rebu98/Rebu98Application.java
@@ -0,0 +1,13 @@
+package com.rebu98.rebu98;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rebu98Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rebu98Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebu98/rebu98/Rebu98ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebu98.rebu98;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rebu98ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
