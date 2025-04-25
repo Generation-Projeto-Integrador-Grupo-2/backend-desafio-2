e0c5f8731e36258a39d4b3b04ae46758a3df68b6
unknown <luanacardosos@outlook.com>
2025-04-25 13:23:48 -0300

    Atualizando o Repository

--- src/main/java/com/rebu98/rebu98/Repositoy/CorridaRepository.java
+++ src/main/java/com/rebu98/rebu98/Repositoy/CorridaRepository.java
@@ -1,5 +1,12 @@
 package com.rebu98.rebu98.Repositoy;
 
-public interface CorridaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.rebu98.rebu98.Model.Corrida;
 
+@Repository
+public interface CorridaRepository extends JpaRepository<Corrida, Long> {
+    List<Corrida> findByUsuarioId(Long usuarioId);
+    List<Corrida> findByMotoristaId(Long motoristaId);
 }
